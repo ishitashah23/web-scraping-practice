8efd190e994b5d546626cf657c7c254ae23f74a8
Ishita Shah <shah.ishita23@gmail.com>
2021-03-20 19:00:02 -0400

    scrapy demo1

---
+++ whiskeyspider.py
@@ -0,0 +1,19 @@
+import scrapy
+from whiskeyscraper.items import WhiskeyscraperItem
+from scrapy.loader import ItemLoader
+
+class WhiskeySpider(scrapy.Spider):
+    name = 'whiskey'
+    start_urls = ['https://www.whiskyshop.com/scotch-whisky?item_availability=In+Stock']
+
+    def parse(self,response):
+        for products in response.css('div.product-item-info'):
+            l = ItemLoader(item=WhiskeyscraperItem(), selector=products)
+            l.add_css('name', 'a.product-item-link')
+            l.add_css('price', 'span.price')
+            l.add_css('link', 'a.product-item-link::attr(href)')
+            yield l.load_item()
+
+            next_page = response.css('a.action.next').attrib['href']
+            if next_page is not None:
+                yield response.follow(next_page, callback=self.parse)
